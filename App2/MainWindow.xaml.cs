5ccd0a7699544a5834b2260a151427e9bafdfb5b
Dmitry Makanov <dmakanov62@gmail.com>
2022-08-14 22:08:57 +0300

    Add acryll to MainWindow

--- App2/MainWindow.xaml.cs
+++ App2/MainWindow.xaml.cs
@@ -9,19 +9,79 @@ using Windows.Storage.Pickers;
 using Windows.Storage;
 using System.Threading;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Composition.SystemBackdrops;
+using WinRT;
+using System.Runtime.InteropServices;
 
 namespace App2
 {
 
+    class WindowsSystemDispatcherQueueHelper
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct DispatcherQueueOptions
+        {
+            internal int dwSize;
+            internal int threadType;
+            internal int apartmentType;
+        }
+
+        [DllImport("CoreMessaging.dll")]
+        private static extern int CreateDispatcherQueueController([In] DispatcherQueueOptions options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object dispatcherQueueController);
+
+        object m_dispatcherQueueController = null;
+        public void EnsureWindowsSystemDispatcherQueueController()
+        {
+            if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
+            {
+                // one already exists, so we'll just use it.
+                return;
+            }
+
+            if (m_dispatcherQueueController == null)
+            {
+                DispatcherQueueOptions options;
+                options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+                options.threadType = 2;    // DQTYPE_THREAD_CURRENT
+                options.apartmentType = 2; // DQTAT_COM_STA
+
+                CreateDispatcherQueueController(options, ref m_dispatcherQueueController);
+            }
+        }
+    }
+
     public sealed partial class MainWindow : Window
     {
         Dictionary<string, string> localSettings;
-        
+
+        WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See separate sample below for implementation
+        Microsoft.UI.Composition.SystemBackdrops.MicaController m_micaController;
+        Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController m_acrylicController;
+        Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration m_configurationSource;
+
+
+
         public MainWindow()
         {
             this.InitializeComponent();
             this.Closed += On_Closed;
             RestoreSettings();
+
+            m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+            m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+            m_acrylicController = new DesktopAcrylicController();
+            m_configurationSource = new SystemBackdropConfiguration();
+
+            m_configurationSource.IsInputActive = true;
+            m_configurationSource.Theme = SystemBackdropTheme.Default;
+            m_acrylicController.TintOpacity = 0.0f;
+            m_acrylicController.LuminosityOpacity = 0.0f;
+            // Enable the system backdrop.
+            // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+            m_acrylicController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+            m_acrylicController.SetSystemBackdropConfiguration(m_configurationSource);
+
 
 
         }
@@ -46,7 +106,7 @@ namespace App2
             if (swloader_combobox.SelectedIndex == 0)
             {
 
-                await Task.Run(()=> SolidWorksAppWorker.OpenSolidWorksApp());
+                await Task.Run(() => SolidWorksAppWorker.OpenSolidWorksApp());
 
             }
             else if (swloader_combobox.SelectedIndex == 1)
@@ -59,7 +119,7 @@ namespace App2
             {
                 Message.ProgressShow(SolidWorksAppWorker.CreateNewDocument,
                         this.Content.XamlRoot, "Создание нового документа");
-                
+
             }
             else
             {
@@ -86,7 +146,7 @@ namespace App2
 
                 if (filepath != null)
                 {
-                    Message.ProgressShow( () => SolidWorksAppWorker.OpenDocument(filepath),
+                    Message.ProgressShow(() => SolidWorksAppWorker.OpenDocument(filepath),
                         this.Content.XamlRoot, "Открытие файла");
                 }
                 else
@@ -165,13 +225,13 @@ namespace App2
         {
             Message.ProgressShow(SolidWorksAppWorker.OpenSolidWorksApp,
                         this.Content.XamlRoot);
-           
+
 
         }
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             SolidWorksAppWorker.CloseSolidWorksApp();
-        }        
+        }
 
     }
 }
