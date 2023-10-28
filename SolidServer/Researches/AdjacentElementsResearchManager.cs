64c6be2c51962b31d5034a49b1a1fb4fa404f80f
makan <dmakanov62@gmail.com>
2023-10-28 12:21:13 +0300

    Add elements printing

--- SolidServer/Researches/AdjacentElementsResearchManager.cs
+++ SolidServer/Researches/AdjacentElementsResearchManager.cs
@@ -2,6 +2,7 @@
 using SolidServer.AreaWorkPackage;
 using System.Collections.Generic;
 using SolidServer.SolidWorksPackage.ResearchPackage;
+using System.Diagnostics;
 
 // Before start open SolidWorks and <3d Part document>
 // with "Simulation" AddIn (NOT "Flow Simulation")
@@ -18,6 +19,15 @@ namespace SolidServer.Researches
             // поиск областей по смежным элементам
             var elems = studyResults.GetElements(wholeNodes,
                Convert.ToInt32(managerConfiguration["nodesIntersectionAmount"])) as HashSet<Element>;
+
+            Console.WriteLine("Вывод на консоль списка всех элементов:");
+            foreach (var elem in elems)
+            {
+                Console.WriteLine(elem.ToString());
+            }
+            Console.WriteLine("Вывод всех элементов окончен!");
+
+
             var newAreas = AreaWorker.DefineAreasPerElementsAdjacent(elems);
             var squeezedAreas = new List<Area>();
             foreach (var area in newAreas)
